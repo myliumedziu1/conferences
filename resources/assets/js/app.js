9113d8f404d0c0c10175dbb474179f14e361ec20
myliumedziu1 <109168470+myliumedziu1@users.noreply.github.com>
2022-11-30 08:47:25 -0800

    Add files via upload

---
+++ resources/assets/js/app.js
@@ -0,0 +1,9 @@
+import 'bootstrap';
+import 'fullcalendar/main';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+
---
+++ resources/views/edit.blade.php
@@ -0,0 +1,15 @@
+@include('layouts.app')
+<div class="col-md-3">
+    <label for="" class="form-label">Event</label>
+    <select class="form-select" id="" required>
+        <option selected disabled value="">Choose...</option>
+        <option>...</option>
+    </select>
+</div>
+
+<div class="col-12">
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="" required>
+        <label class="form-check-label" for="#">
+            Agree to terms and conditions
+        </label>
